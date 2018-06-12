c50c7116c7ecc43ca79077a0c9ee16e7277ac520
cesteban <carmenestebanc@gmail.com>
2018-06-12 09:01:29 -0400

    12062018 dev

--- src/app/theme/components/header/header.component.ts
+++ src/app/theme/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'ubl-header',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
